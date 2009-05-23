269ecf983df13a6643ecbbefbc20ec55db38cf64
Jaime Iniesta <jaimeiniesta@gmail.com>
2009-05-23 18:15:57 +0200

    Removed format.xml from generated scaffolds as I dont plan using xml right now

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,36 +3,30 @@ class EntriesController < ApplicationController
   after_filter :admin_login
     
   # GET /entries
-  # GET /entries.xml
   def index
     @entries = Entry.all(:order => 'published desc')
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @entries }
     end
   end
 
   # GET /entries/1
-  # GET /entries/1.xml
   def show
     @entry = Entry.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @entry }
     end
   end
 
   # DELETE /entries/1
-  # DELETE /entries/1.xml
   def destroy
     @entry = Entry.find(params[:id])
     @entry.destroy
 
     respond_to do |format|
       format.html { redirect_to(entries_url) }
-      format.xml  { head :ok }
     end
   end
 end
--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -3,35 +3,29 @@ class FeedsController < ApplicationController
   after_filter :admin_login
   
   # GET /feeds
-  # GET /feeds.xml
   def index
     @feeds = Feed.all(:order => 'last_modified desc')
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @feeds }
     end
   end
 
   # GET /feeds/1
-  # GET /feeds/1.xml
   def show
     @feed = Feed.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @feed }
     end
   end
 
   # GET /feeds/new
-  # GET /feeds/new.xml
   def new
     @feed = Feed.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @feed }
     end
   end
 
@@ -41,7 +35,6 @@ class FeedsController < ApplicationController
   end
 
   # POST /feeds
-  # POST /feeds.xml
   def create
     @feed = Feed.new(params[:feed])
 
@@ -49,16 +42,13 @@ class FeedsController < ApplicationController
       if @feed.save
         flash[:notice] = 'Feed was successfully created.'
         format.html { redirect_to(@feed) }
-        format.xml  { render :xml => @feed, :status => :created, :location => @feed }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @feed.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # PUT /feeds/1
-  # PUT /feeds/1.xml
   def update
     @feed = Feed.find(params[:id])
 
@@ -66,23 +56,19 @@ class FeedsController < ApplicationController
       if @feed.update_attributes(params[:feed])
         flash[:notice] = 'Feed was successfully updated.'
         format.html { redirect_to(@feed) }
-        format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @feed.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # DELETE /feeds/1
-  # DELETE /feeds/1.xml
   def destroy
     @feed = Feed.find(params[:id])
     @feed.destroy
 
     respond_to do |format|
       format.html { redirect_to(feeds_url) }
-      format.xml  { head :ok }
     end
   end
 end
