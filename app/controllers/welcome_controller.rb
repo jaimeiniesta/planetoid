7d5c9a5a32eae6e4133c227cecd5c900b1d27531
nickel <juan@beecoder.com>
2009-05-28 23:26:49 +0200

    Eager loading for a little improve in perfomance

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
   def index
     # TODO: maybe it would be better to sort them by activity, showing first the ones with recent posts
     @users = User.find(:all, :order => 'name')
-    @entries = Entry.find(:all, :limit => 25, :order => 'published desc')
+    @entries = Entry.find(:all, :limit => 25, :order => 'published desc', :include => {:feed => :user})
   end
 
 end
