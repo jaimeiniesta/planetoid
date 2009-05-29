466ed6fa520e6ec902b8364129e1bb133255878a
nickel <juan@beecoder.com>
2009-05-29 18:32:57 +0200

    Update old github and twitter URLs

--- db/migrate/20090528214049_change_users_rename_twitter_and_github_columns.rb
+++ db/migrate/20090528214049_change_users_rename_twitter_and_github_columns.rb
@@ -4,12 +4,18 @@ class ChangeUsersRenameTwitterAndGithubColumns < ActiveRecord::Migration
       t.rename :twitter_url, :twitter_user
       t.rename :github_url, :github_user
     end
+    
+    User.all.each do |user|
+      user.github_user.gsub!(GITHUB_URL, '')
+      user.twitter_user.gsub!(TWITTER_URL, '')
+      user.save
+    end
   end
 
   def self.down
     change_table :users do |t|
       t.rename :twitter_user, :twitter_url
       t.rename :github_user, :github_url
-    end
+    end  
   end
 end
