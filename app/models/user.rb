e0b762fcde73732dd1cff6a1dadd7c8093119a83
Jaime Iniesta <jaimeiniesta@gmail.com>
2009-06-07 02:15:09 +0200

    User email is no longer mandatory

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ActiveRecord::Base
   
   has_and_belongs_to_many :projects, :order => 'projects.name'
   
-  validates_presence_of :name, :email
-  validates_uniqueness_of :email
-  validates_uniqueness_of :blog_url, :twitter_user, :github_user, :allow_blank => true
-  validates_format_of :email, :with => REXP_EMAIL
+  validates_presence_of :name
+
+  validates_format_of :email, :with => REXP_EMAIL, :allow_blank => true
   validates_format_of :blog_url, :with => REXP_URL, :allow_blank => true
   validates_format_of :twitter_user, :github_user, :with => REXP_USER, :allow_blank => true
+  
+  validates_uniqueness_of :email, :allow_blank => true
+  validates_uniqueness_of :blog_url, :twitter_user, :github_user, :allow_blank => true  
   
   # Returns the full github URL for this user if has a github user, or nil if not
   def github_url
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -17,9 +17,8 @@ class UserTest < ActiveSupport::TestCase
   def test_should_validate_presence_of_required_fields
     user = User.new
     assert !user.valid?
-    assert_equal user.errors.size, 3
+    assert_equal user.errors.size, 1
     assert user.errors.on(:name)  # cant be blank
-    assert user.errors.on(:email) # cant be blank, format invalid
     
     user.name = "Pepito Grillo"
     user.email = "pepito@example.com"
@@ -47,13 +46,13 @@ class UserTest < ActiveSupport::TestCase
   
   def test_email_should_have_a_valid_format
     user = create_user
-    [nil, 'gmail', 'gmail.com', 'jaimeiniesta@gmail', '@gmail.com', 'jaimeiniesta @ gmail.com'].each do |s|
+    ['gmail', 'gmail.com', 'jaimeiniesta@gmail', '@gmail.com', 'jaimeiniesta @ gmail.com'].each do |s|
       user.email = s
       assert !user.valid?
       assert user.errors.on(:email)
     end
     
-    ['jaimeiniesta@gmail.com', 'pepe@example.com', 'JAIME@EXAMPLE.COM', 'jaime_iniesta@gmail.com', 'jaime.iniesta@gmail.com'].each do |s|
+    [nil, '', 'jaimeiniesta@gmail.com', 'pepe@example.com', 'JAIME@EXAMPLE.COM', 'jaime_iniesta@gmail.com', 'jaime.iniesta@gmail.com'].each do |s|
       user.email = s
       assert user.valid?
       assert !user.errors.on(:email)
