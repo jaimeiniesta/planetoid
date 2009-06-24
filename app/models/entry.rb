57d592ec501869a46facf4e6763ede339c4de8d0
javier <javier.ramirez.gomara@gmail.com>
2009-06-24 13:43:56 +0200

    twitter updates

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,4 +3,11 @@ class Entry < ActiveRecord::Base
   
   validates_presence_of :feed_id, :url
   validates_uniqueness_of :url
+  
+  def after_create
+    if PLANETOID_CONF[:twitter][:send_updates] && self.published > self.feed.created_at
+      twit=Twitter::Base.new PLANETOID_CONF[:twitter][:user],PLANETOID_CONF[:twitter][:password]
+      twit.update "#{PLANETOID_CONF[:twitter][:update_prefix]} #{self.title[0..150]} #{self.url}"
+    end
+  end
 end
