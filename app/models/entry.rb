507acec38f19e8cea72a5c54be2918fbfa65c6b3
Jaime Iniesta <jaimeiniesta@gmail.com>
2009-06-28 12:21:59 +0200

    Required jnunemaker-twitter 0.6.12

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,10 +4,11 @@ class Entry < ActiveRecord::Base
   validates_presence_of :feed_id, :url
   validates_uniqueness_of :url
   
+  # Send a twitter notification if necessary
   def after_create
     if PLANETOID_CONF[:twitter][:send_updates] && self.published > self.feed.created_at
-      twit=Twitter::Base.new PLANETOID_CONF[:twitter][:user],PLANETOID_CONF[:twitter][:password]
-      twit.update "#{PLANETOID_CONF[:twitter][:update_prefix]} #{self.title[0..150]} #{self.url}"
+      twit=Twitter::HTTPAuth.new PLANETOID_CONF[:twitter][:user], PLANETOID_CONF[:twitter][:password]
+      twit.update "#{PLANETOID_CONF[:twitter][:update_prefix]} #{self.title[0..150]} #{self.url}" rescue nil
     end
   end
 end
