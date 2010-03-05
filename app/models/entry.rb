f311a37edbe03fa7606efc521bd46f56c004f8a3
Jaime Iniesta <jaimeiniesta@gmail.com>
2010-03-05 11:35:02 +0100

    Entry generates twitter messages as prefix + title + url, truncating as needed to fit into 140 chars max.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -5,19 +5,37 @@ class Entry < ActiveRecord::Base
   validates_uniqueness_of :url
 
   after_create :twitt
-
-  private
   
   # Send a twitter notification if necessary
   def twitt
     if PLANETOID_CONF[:twitter][:entries][:send_twitts] && self.published > self.feed.created_at
       begin
         twit=Twitter::Base.new(Twitter::HTTPAuth.new(PLANETOID_CONF[:twitter][:user], PLANETOID_CONF[:twitter][:password]))
-        twit.update "#{PLANETOID_CONF[:twitter][:entries][:prefix]} #{self.title[0..150]} #{self.url}"
+        twit.update twitter_msg
       rescue Exception => e
         puts e.message
         puts e.backtrace.inspect
       end
     end
   end
+  
+  # Twitter message should be always 140 chars max.
+  # Twitter message is generated as prefix + title + url, in this preference order and truncating as needed
+  # Prefix will be truncated and take the full 140 chars if needed
+  # Title will only be included if there is room for prefix, an space and at least 5 chars for title
+  # URL will only be included if there is enough room for prefix, title and itself without truncating
+  def twitter_msg    
+    msg = PLANETOID_CONF[:twitter][:entries][:prefix][0..139]
+    
+    if msg.length < 135
+      msg = msg + " " + self.title[0..(140 - msg.length - 2)]
+      
+      if msg.length < 140 && self.url.length < (140 - msg.length)
+        msg = msg + " " + self.url
+      end
+    end
+
+    msg
+  end
+  
 end
--- test/unit/entry_test.rb
+++ test/unit/entry_test.rb
@@ -14,6 +14,70 @@ class EntryTest < ActiveSupport::TestCase
     end
   end
   
+  def test_should_generate_twitter_msg_as_prefix_title_url
+    prefix = PLANETOID_CONF[:twitter][:entries][:prefix]
+    entry = new_entry
+
+    assert_equal 46, entry.twitter_msg.length
+    assert_equal "#{prefix} #{entry.title} #{entry.url}", entry.twitter_msg
+  end
+
+  def test_should_use_only_prefix_if_no_room_for_title
+    prefix = PLANETOID_CONF[:twitter][:entries][:prefix] = "A"*140
+    entry = new_entry
+    
+    assert_equal 140, entry.twitter_msg.length
+    assert_equal "#{prefix}", entry.twitter_msg
+  end
+
+  def test_should_truncate_prefix_if_needed
+    prefix = PLANETOID_CONF[:twitter][:entries][:prefix] = "A"*141
+    entry = new_entry
+    
+    assert_equal 140, entry.twitter_msg.length
+    assert_equal "#{prefix[0..139]}", entry.twitter_msg
+  end
+  
+  def test_should_not_include_title_if_there_is_no_space_for_prefix_one_space_and_5_chars_for_title
+    prefix = PLANETOID_CONF[:twitter][:entries][:prefix] = "A"*135
+    entry = new_entry(:title => "B"*5)
+    
+    assert_equal 135, entry.twitter_msg.length
+    assert_equal "#{prefix}", entry.twitter_msg
+  end
+  
+  def test_should_include_title_if_there_is_space_for_prefix_one_space_and_5_chars_for_title
+    prefix = PLANETOID_CONF[:twitter][:entries][:prefix] = "A"*134
+    entry = new_entry(:title => "B"*5)
+    
+    assert_equal 140, entry.twitter_msg.length
+    assert_equal "#{prefix} #{entry.title}", entry.twitter_msg
+  end
+  
+  def test_should_truncate_title_if_needed
+    prefix = PLANETOID_CONF[:twitter][:entries][:prefix] = "A"*130
+    entry = new_entry(:title => "B"*10)
+    
+    assert_equal 140, entry.twitter_msg.length
+    assert_equal "#{prefix} #{entry.title[0..8]}", entry.twitter_msg
+  end
+  
+  def test_should_include_url_if_there_is_enough_room
+    prefix = PLANETOID_CONF[:twitter][:entries][:prefix] = "A"*111
+    entry = new_entry(:title => "B"*5)
+    
+    assert_equal 140, entry.twitter_msg.length
+    assert_equal "#{prefix} #{entry.title} #{entry.url}", entry.twitter_msg
+  end
+  
+  def test_should_not_include_url_if_there_is_not_enough_room
+    prefix = PLANETOID_CONF[:twitter][:entries][:prefix] = "A"*112
+    entry = new_entry(:title => "B"*5)
+    
+    assert_equal 118, entry.twitter_msg.length
+    assert_equal "#{prefix} #{entry.title}", entry.twitter_msg
+  end
+  
   private
   
   def create_entry(options = {})
@@ -27,6 +91,17 @@ class EntryTest < ActiveSupport::TestCase
                         :categories => 'ruby, rails, web development'}.merge(options))
     record.save
     record
+  end
+  
+  def new_entry(options = {})
+    record = Entry.new({:feed_id => feeds(:jaime_blog).id,
+                        :url => 'http://www.example.com',
+                        :author => 'Jaime Iniesta',
+                        :title => 'Another entry',
+                        :summary => 'The summary',
+                        :content => 'The content is usually longer than the summary',
+                        :published => 3.days.ago,
+                        :categories => 'ruby, rails, web development'}.merge(options))
   end
   
 end
